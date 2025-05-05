3a84412be94f7c6a8008f93e09dda7ed74bb0fe6
Simón García <157841549+SimonGarcia01@users.noreply.github.com>
2025-05-04 22:09:04 -0500

    Added the isntructions for a more robust page that handles users in a small data base. This is the backend part.

--- API-REST/src/index.js
+++ API-REST/src/index.js
@@ -1,35 +1,24 @@
 const express = require("express")
-const user = require("./control/user.js")
+const user = require("./control/usercontroller")
 
 const app = express()
+
 app.use(express.json())
 
 const port = 5000
 
-// Creando los endpoints para la gestion de usuarios:
 app.get("/users", user.list)
 
 app.get("/users/:id", user.get)
 
-app.post("/users/:id", user.create)
+app.post("/users/", user.create)
 
-// método | verbo GET
-app.get("/", (req, res) => {
-  // 200 -> OK : https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
-    res.status(200).send("Hola desde el API")  
+app.put("/users/:id", user.update)
+
+app.delete("/users/:id", user.delete)
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '..', 'public' , 'notfound.html'))
 })
 
-app.post("/", (req, res) => {
-   // 201 -> OK -> Data Created : https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
-    res.status(201).send("element created")  
-})
-
-app.put("/", (req, res) => {
-    res.status(204).send("NO Content in edit")  
-})
-
-app.delete("/", (req, res) => {
-    res.status(204).send("NO content in delete")  
-})
 app.listen(port)
-
