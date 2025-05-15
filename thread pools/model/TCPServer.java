e953a91ed9e4e552d21909212de3662dd52cf850
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2025-05-15 10:52:24 -0500

    working a basic server structure with threadpool

---
+++ thread pools/model/TCPServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TCPServer {
+    public static void main(String[] args) {
+        
+        //Create the thread pools to get the info from clients
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            //executes one thread
+            //Has to accept some type of connection
+            //Socket socket = serverSocket.accept();
+            // pool.execute(new ClientHandler(socket));
+
+            //Execute multiple threads
+            while (true) {
+                Socket socket = serverSocket.accept();
+                pool.execute(new ClientHandler(socket));
+            }
+            
+            
+
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+}
